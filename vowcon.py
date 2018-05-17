48a7a8666be3d147cfc54b0149a8fbcccdf35be4
thusyasin <36623598+thusyasin@users.noreply.github.com>
2018-05-17 11:08:42 +0530

    Create vowcon.py

---
+++ vowcon.py
@@ -0,0 +1,5 @@
+ch=input()
+if (ch=='a' or ch=='A' or ch=='e' or ch=='E' or ch=='i' or ch=='I' or ch=='o' or ch=='O' or ch=='u' or ch=='U'):
+print "letter is vowel"
+else:
+print "Letter is consonant"
