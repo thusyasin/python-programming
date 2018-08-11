9b6537722297aae9e094ab9b3ac6bccb0aa58b5e
thusyasin <36623598+thusyasin@users.noreply.github.com>
2018-08-11 07:12:23 -0700

    Create app.java

---
+++ app.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class app{
+public static void main(String ar[]){
+Scanner s=new Scanner(System.in);
+String s1=s.nextLine();
+StringBuilder ss=new StringBuilder(s1);
+s1.append(".");
+System.out.print(s1);
+}
+}
