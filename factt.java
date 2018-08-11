5fd55647d2ff95b92cdcb50d6ad34bbe14494496
thusyasin <36623598+thusyasin@users.noreply.github.com>
2018-08-11 06:27:50 -0700

    Create factt.java

---
+++ factt.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class factt{
+public static void main(string ar[]){
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+int fact=1,i;
+for(i=1;i<=n;i++){
+fact=fact*i;
+}
+System.out.print(fact);
+}
+}
