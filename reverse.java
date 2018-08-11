a6f81e878b1883cc00323c4f0ee43377ab0c9efb
thusyasin <36623598+thusyasin@users.noreply.github.com>
2018-08-11 06:22:12 -0700

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -6,7 +6,7 @@ Scanner s=new Scanner(System.in);
 String s1=s.nextLine();
 char c[]=s1.toCharArray();
 for(int i=c.length-1;i>=0;i--){
-System.out.print(s.charAt(i));
+System.out.print(s1.charAt(i));
 }
 }
 }
