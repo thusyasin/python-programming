a9931bc17daf563c3a6441f0dea62866fabf4adf
thusyasin <36623598+thusyasin@users.noreply.github.com>
2018-08-12 08:17:51 -0700

    Create squ.java

---
+++ squ.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class squ{
+public static void main(String ar[]){
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+int i,sum=0;
+int b[]=new int[50];
+String s=Integer.toString(n);
+for(i=0;i<s.length();i++){
+a=Integer.parseInt(Character.toString(s.charAt(i)));
+b[i]=a*a;
+sum=sum+b[i];
+}
+System.out.print(sum);
+}
+}
